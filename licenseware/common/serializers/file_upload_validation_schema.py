d5ab55d749c15d0ecbafd79b461da7d7367c4b78
ClimenteA <climente.alin@gmail.com>
2021-09-10 11:51:47 +0300

    fix: removed states.FAIL references from sdk with states.FAILED

--- licenseware/common/serializers/file_upload_validation_schema.py
+++ licenseware/common/serializers/file_upload_validation_schema.py
@@ -9,12 +9,12 @@ from licenseware.common.constants import states
 
 class FileValidationSchema(Schema):
     filename = fields.Str(required=True, validate=validate.Length(min=5))
-    status   = fields.Str(required=True, validate=validate.OneOf(states.SUCCESS, states.FAIL)) 
+    status   = fields.Str(required=True, validate=validate.OneOf(states.SUCCESS, states.FAILED)) 
     message  = fields.Str(required=True, validate=validate.Length(min=10))
     
 
 class FileUploadValidationSchema(Schema):
-    status = fields.Str(required=True, validate=validate.OneOf(states.SUCCESS, states.FAIL)) 
+    status = fields.Str(required=True, validate=validate.OneOf(states.SUCCESS, states.FAILED)) 
     message = fields.Str(required=True, validate=validate.Length(min=10))
     validation = fields.List(fields.Nested(FileValidationSchema), required=False, allow_none=True)
     units = fields.Int(requred=False, allow_none=True)
