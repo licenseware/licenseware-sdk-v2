e0765572cd2d35d9d0366d39a4d3b4b878d6cb32
ClimenteA <climente.alin@gmail.com>
2021-10-01 15:36:00 +0300

    fix: updated endpoint builder flask request

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: uwsgi --http 0.0.0.0:5000 -w main:app --processes 4
-worker: dramatiq main:App.broker -p4 --queues test
--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Making sure aws updates requirements
-pip uninstall -y licenseware && pip install -r requirements.txt 
-
-honcho start
