c96b31d03c93faf946dcd93a8b7e018f075ea973
Ciprian <ciprian@licenseware.io>
2021-10-07 11:08:17 +0300

    feat: added column type to filter component

--- licenseware/common/serializers/report_component_filter_schema.py
+++ licenseware/common/serializers/report_component_filter_schema.py
@@ -6,4 +6,5 @@ class FilterSchema(Schema):
     column = fields.Str(required=True)
     allowed_filters = fields.List(fields.Str, required=True)
     visible_name = fields.Str(required=True)
+    column_type = fields.Str(required=True)
     
