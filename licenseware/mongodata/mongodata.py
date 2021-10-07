bf978252055a44b8408e1eb65dd028f70607f4ce
ClimenteA <climente.alin@gmail.com>
2021-10-07 10:07:11 +0300

    chore: commented logs

--- licenseware/mongodata/mongodata.py
+++ licenseware/mongodata/mongodata.py
@@ -252,7 +252,7 @@ def insert(schema, collection, data, db_name=None):
     collection_name = return_collection_name(collection)
     with Connect.get_connection() as mongo_connection:
         collection = mongo_connection[db_name][collection_name]
-        log.debug(collection)
+        # log.debug(collection)
         if not isinstance(collection, Collection):
             return collection
 
@@ -299,7 +299,7 @@ def fetch(match:dict, collection:str, as_list:bool = True, limit:int = None, ski
     collection_name = return_collection_name(collection)
     with Connect.get_connection() as mongo_connection:
         collection = mongo_connection[db_name][collection_name]
-        log.debug(collection)
+        # log.debug(collection)
         if not isinstance(collection, Collection):
             return collection
 
@@ -446,7 +446,7 @@ def update(schema, match, new_data, collection, append=False, db_name=None):
     collection_name = return_collection_name(collection)
     with Connect.get_connection() as mongo_connection:
         collection = mongo_connection[db_name][collection_name]
-        log.debug(collection)
+        # log.debug(collection)
         match = parse_match(match)
         match = match['query'] or match['_id']
         if not match:
