6491da23b904824922ed359fb766e4bda88759ee
Ciprian <ciprian@licenseware.io>
2021-10-11 13:46:32 +0300

    Revert "fix: no need for type on metaspecs"
    This reverts commit 4f2c92fa399f24f37db39d6fb1084e00c2d95c1b.

--- licenseware/editable_table/metaspecs.py
+++ licenseware/editable_table/metaspecs.py
@@ -5,14 +5,16 @@ def metaspecs(
     editable:bool = False, 
     visible:bool = True,
     distinct_key:str = None, 
-    foreign_key:str = None
+    foreign_key:str = None,
+    type:str = 'string'
 ):
    
     return {
         'editable': editable,
         'visible': visible,
         'distinct_key': distinct_key,
-        'foreign_key': foreign_key
+        'foreign_key': foreign_key,
+        'type': type
     }
     
     
