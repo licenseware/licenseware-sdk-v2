762184df2e2bfcade5cc8928cc003dcd4eba4b6a
ClimenteA <climente.alin@gmail.com>
2021-10-20 11:10:43 +0300

    feature: added type to attributes_table function

--- licenseware/report_components/attributes/table.py
+++ licenseware/report_components/attributes/table.py
@@ -13,24 +13,53 @@ def attributes_table(props:List[str]):
     {'columns': [
         {
             'name': 'Number of databases', 
-            'prop': 'number_of_databases'
+            'prop': 'number_of_databases',
+            'type': 'string'
         },
         {
             'name': 'Edition', 
-            'prop': 'edition'
+            'prop': 'edition',
+            'type': 'string'
         }
     ]}
+    
+    From this input: 
+    props = [('number_of_databases', 'number'), 'edition'] 
+    
+    
+    The result will be a dictionary like bellow:
+    
+    {'columns': [
+        {
+            'name': 'Number of databases', 
+            'prop': 'number_of_databases',
+            'type': 'number'
+        },
+        {
+            'name': 'Edition', 
+            'prop': 'edition',
+            'type': 'string'
+        }
+    ]}
+    
+    If given a tuple the second value from the tuple will be set as type for that column.
+    By default typle will be string.
     
     """
     
     prop_columns = {'columns': []}
   
     for prop in props:
-        prop_columns['columns'].append({
-            'name': re.sub('_', ' ', prop).capitalize(),
-            'prop': prop
-        })
         
+        name = prop[0] if isinstance(prop, tuple) else prop
+        col_type = prop[1] if isinstance(prop, tuple) else 'string'
+        
+        prop_columns['columns'].append({
+            'name': re.sub('_', ' ', name).capitalize(),
+            'prop': name,
+            'type': col_type
+        })
+    
     return prop_columns
         
             
