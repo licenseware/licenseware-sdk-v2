712f54f71f8f9f53ae04f8560f0a544e2ef79d3c
Ciprian <ciprian@licenseware.io>
2021-11-04 21:38:57 +0200

    fix: create df from records

--- licenseware/download/download_xlsx.py
+++ licenseware/download/download_xlsx.py
@@ -18,7 +18,7 @@ def download_as_xlsx(data:List[dict], tenant_id:str, filename:str = None, send_f
     if not os.path.exists(dirpath): os.makedirs(dirpath)
     
     filepath = os.path.join(dirpath, filename)
-    df = pd.DataFrame(data, index=[0])
+    df = pd.DataFrame.from_records(data)
 
     if not send_file: return df
         
@@ -30,4 +30,3 @@ def download_as_xlsx(data:List[dict], tenant_id:str, filename:str = None, send_f
         as_attachment=True
     )
         
-    
