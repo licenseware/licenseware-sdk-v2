8e271ba42f5c72ad0cf688bd881b737b0de7f125
ClimenteA <climente.alin@gmail.com>
2021-11-11 13:14:40 +0200

    feature: updated main.py template with av scan

--- licenseware/resources/_main.py
+++ licenseware/resources/_main.py
@@ -1,10 +1,10 @@
-# from dotenv import load_dotenv
-# load_dotenv()  
+import os
+from dotenv import load_dotenv
+if os.getenv("APP_ID") is None: load_dotenv()
 
 from trend_app_protect import wrap_wsgi_app
 from flask import Flask
 from app import App
-
 
 
 app = Flask(__name__)
@@ -12,9 +12,10 @@ app = Flask(__name__)
 App.init_app(app)
 App.register_app()
 
-# Trend Micro `Application Security`
-# app = wrap_wsgi_app(app)
 
+# Trend Micro `Application Security`
+if os.getenv('ENVIRONMENT') in {'prod', 'production'}: 
+    app = wrap_wsgi_app(app)
 
 
 
