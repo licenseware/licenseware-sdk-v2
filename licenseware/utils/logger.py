72c605b44aa54591bfd8aef31048b7a3b2ff87c8
ClimenteA <climente.alin@gmail.com>
2021-12-10 12:16:29 +0200

    chore: reverted to previous logger temporary

--- licenseware/utils/logger.py
+++ licenseware/utils/logger.py
@@ -61,8 +61,13 @@ from loguru import logger as log
 _debug = os.getenv('DEBUG', '').lower() == 'true'
 _log_level = 'DEBUG' if _debug else 'WARNING'
 
-_log_format = """<yellow>Tenantid={extra[tenant_id]}</yellow>
-<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>[ <level>{level}</level> ]
+# _log_format = """<yellow>Tenantid={extra[tenant_id]}</yellow>
+# <green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>[ <level>{level}</level> ]
+# <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>
+# <level>{message}</level>
+# """
+
+_log_format = """<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>[ <level>{level}</level> ]
 <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>
 <level>{message}</level>
 """
@@ -72,9 +77,9 @@ try:
 except:
     pass#No default logger
 
-log.configure(patcher=lambda record: record["extra"].update(
-    tenant_id=request.headers.get("Tenantid", "")) if has_request_context() else "",
-)
+# log.configure(patcher=lambda record: record["extra"].update(
+#     tenant_id=request.headers.get("Tenantid", "")) if has_request_context() else "",
+# )
 
 log.add(
     "app.log", 
