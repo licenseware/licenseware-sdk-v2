2a4b3a79054aa5862cb8d94046573329f6905577
cgr <cipriangrigore.1@gmail.com>
2022-02-11 22:19:34 +0200

    feat: add logging for file processing (#43)
    Co-authored-by: Ciprian <ciprian@licenseware.io>

--- licenseware/notifications/notify_upload_status.py
+++ licenseware/notifications/notify_upload_status.py
@@ -3,19 +3,23 @@ from licenseware.utils.logger import log
 from licenseware.common.constants import envs
 
 
-def notify_upload_status(event: dict, status:str):
+def notify_upload_status(event: dict, status: str):
     """
         Notify registry about uploader processing status
     """
-    
+
     status = {
         'tenant_id': event['tenant_id'],
-        'uploader_id': event['uploader_id'], 
+        'uploader_id': event['uploader_id'],
         'status': status,
         'app_id': envs.APP_ID
     }
-    
+
+    log.info(
+        f"APP PROCESSING EVENT: {envs.APP_ID} in status {status} for uploader {event['uploader_id']} for tenant event['tenant_id']")
+
     response, status_code = register_upload_status(**status)
-    
-    if status_code != 200: return False
+
+    if status_code != 200:
+        return False
     return True
