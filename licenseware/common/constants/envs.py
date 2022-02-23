f76fbb5b089eff1ee62b4b56ee2dd4b11887996a
Meysam <MeysamAzad81@gmail.com>
2022-02-23 22:50:22 +0300

    fix: receive none as default redis password 🐛 (#56)

--- licenseware/common/constants/envs.py
+++ licenseware/common/constants/envs.py
@@ -89,7 +89,7 @@ class envs:
     REDIS_HOST: str = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT: int = int(os.getenv("REDIS_PORT", "6379"))
     REDIS_DB: int = int(os.getenv("REDIS_DB", "0"))
-    REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD", "")
+    REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD")
 
     REDIS_RESULT_CACHE_DB: int = int(os.getenv("REDIS_RESULT_CACHE_DB", "15"))
 
