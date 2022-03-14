b3527dc36ee07d81c70d2c049ea67debcb89611e
ClimenteA <climente.alin@gmail.com>
2022-03-14 16:30:40 +0200

    chore: testing

--- tests/test_history.py
+++ tests/test_history.py
@@ -9,58 +9,120 @@ class TestHistory(unittest.TestCase):
 
     def test_save_filename_validation(self):
 
-        @History.log()
-        def validate_filenames(flask_request, name, data):
-            """ Validate filenames received """
 
-            response, status_code = {
-                        "status": "success",
-                        "message": "Filenames are valid",
-                        "data": f"{name}, {data}",
-                        "validation": [
-                            {
-                              "status": "success",
-                              "filename": "rvtools.xlsx",
-                              "message": "Filename is valid"
-                            },
-                            {
-                              "status": "success",
-                              "filename": "options.csv",
-                              "message": "Filename is valid"
-                            }
-                          ]
-                    }, 200
+        class UploaderBuilder:
 
-            return response, status_code
+            def __init__(self, uploader_id):
+                self.uploader_id = uploader_id
 
-        request = get_flask_request(headers={
-            'TenantId': "b37761e3-6926-4cc1-88c7-4d0478b04adf",
-            'Authorization': "e9898dfl4s34kjs",
-            # 'UploaderId': "rv_tools",
-            "X-Forwarded-Path": '/universal-uploader/uploads/universal_uploader/validation'
-        })
+            @History.log()
+            def validate_filenames(self, flask_request):
+                """ Validate file names provided by user """
 
-        response, status_code, headers = validate_filenames(request, "the string", data=[1, 2, 3])
+                assert self.uploader_id
 
-        self.assertEqual(status_code, 200)
-        self.assertEqual(response['status'], 'success')
+                response = {
+                  "status": "success",
+                  "message": "Filenames are valid",
+                  "validation": [
+                    {
+                      "status": "success",
+                      "filename": "rvtools.xlsx",
+                      "message": "Filename is valid"
+                    },
+                    {
+                      "status": "success",
+                      "filename": "options.csv",
+                      "message": "Filename is valid"
+                    }
+                  ]
+                }
 
-        @History.log()
-        def processing_function(name, data, filepath,
-                                tenant_id="b37761e3-6926-4cc1-88c7-4d0478b04adf",
-                                event_id="b37761e3-6926-4cc1-88c7-4d0478b04adf",
-                                uploader_id="rv_tools"):
-            """ Process data from file """
+                status_code = 200
 
-            response, status_code = {
-                                        "status": "success",
-                                        "message": "Filenames are valid",
-                                        "data": f"{name}, {data}"
-                                    }, 200
+                return response, status_code
 
-            return response, status_code
 
-        response, status_code = processing_function("Alin", [1, 2, 3], filepath='path/to/file')
+            def upload_files(self, flask_request, event_id=None):
+                """ Validate file content provided by user and send files for processing if they are valid """
 
-        self.assertEqual(status_code, 200)
-        self.assertEqual(response['status'], 'success')
+                header_event_id = flask_request.headers.get("EventId") or event_id
+                if header_event_id is None:
+                    raise Exception("Parameter `EventId` not provided in headers")
+
+                    return {'status': states.SUCCESS, 'message': 'Event sent', 'event_data': events_data}, 200
+                else:
+                    event.update({
+                        'filepaths': valid_filepaths,
+                        'flask_request': {**flask_json, **flask_headers, **flask_args},
+                        'validation_response': response
+                    })
+
+                    if not validate_event(event, raise_error=False):
+                        log.error(event)
+                        notify_upload_status(event, status=states.IDLE)
+                        return {'status': states.FAILED, 'message': 'Event not valid', 'event_data': event}, 400
+
+                    log.info("Sending event: " + str(event))
+                    self.worker.send(event)
+
+                    return {'status': states.SUCCESS, 'message': 'Event sent', 'event_data': event}, 200
+
+
+
+
+        # @History.log()
+        # def validate_filenames(flask_request, name, data):
+        #     """ Validate filenames received """
+        #
+        #     response, status_code = {
+        #                 "status": "success",
+        #                 "message": "Filenames are valid",
+        #                 "data": f"{name}, {data}",
+        #                 "validation": [
+        #                     {
+        #                       "status": "success",
+        #                       "filename": "rvtools.xlsx",
+        #                       "message": "Filename is valid"
+        #                     },
+        #                     {
+        #                       "status": "success",
+        #                       "filename": "options.csv",
+        #                       "message": "Filename is valid"
+        #                     }
+        #                   ]
+        #             }, 200
+        #
+        #     return response, status_code
+        #
+        # request = get_flask_request(headers={
+        #     'TenantId': "b37761e3-6926-4cc1-88c7-4d0478b04adf",
+        #     'Authorization': "e9898dfl4s34kjs",
+        #     # 'UploaderId': "rv_tools",
+        #     "X-Forwarded-Path": '/universal-uploader/uploads/universal_uploader/validation'
+        # })
+        #
+        # response, status_code, headers = validate_filenames(request, "the string", data=[1, 2, 3])
+        #
+        # self.assertEqual(status_code, 200)
+        # self.assertEqual(response['status'], 'success')
+        #
+        # @History.log()
+        # def processing_function(name, data, filepath,
+        #                         tenant_id="b37761e3-6926-4cc1-88c7-4d0478b04adf",
+        #                         event_id="b37761e3-6926-4cc1-88c7-4d0478b04adf",
+        #                         uploader_id="rv_tools"):
+        #     """ Process data from file """
+        #
+        #     response, status_code = {
+        #                                 "status": "success",
+        #                                 "message": "Filenames are valid",
+        #                                 "data": f"{name}, {data}"
+        #                             }, 200
+        #
+        #     return response, status_code
+        #
+        # response, status_code = processing_function("Alin", [1, 2, 3], filepath='path/to/file')
+        #
+        # self.assertEqual(status_code, 200)
+        # self.assertEqual(response['status'], 'success')
