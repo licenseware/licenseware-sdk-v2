8bc08a78c9d33507701d9f621b16d58b6530db6d
ClimenteA <climente.alin@gmail.com>
2022-03-15 13:17:13 +0200

    fix: added event_id on worker payload schema

--- licenseware/common/serializers/event_schema.py
+++ licenseware/common/serializers/event_schema.py
@@ -4,6 +4,7 @@ from licenseware.common.validators import validate_uuid4
 
 class EventSchema(Schema):
     tenant_id = fields.Str(required=True, validate=validate_uuid4)
+    event_id = fields.Str(required=True, validate=validate_uuid4)
     uploader_id = fields.Str(required=True, validate=validate.Length(min=3))
     filepaths = fields.List(fields.Str, required=True)
     flask_request = fields.Dict(required=False, allow_none=True)
