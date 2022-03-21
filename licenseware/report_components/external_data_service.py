73f89f708f401725b1b0f65e39c3a522eb898d2b
Adrian Mihalache <adrian.m@licenseware.io>
2022-03-21 16:36:13 +0200

    use type for components & grab component url from registry service

--- licenseware/report_components/external_data_service.py
+++ licenseware/report_components/external_data_service.py
@@ -1,16 +1,43 @@
 import requests
+import traceback
+import os
+
 from licenseware.utils.logger import log
 
 
 class ExternalDataService:
     @staticmethod
+    def _get_all_components(headers):
+        registry_service_url = os.get("REGISTRY_SERVICE_URL")
+        try:
+            comp_data = requests.get(
+                url=f"{registry_service_url}/components"
+            )
+            return comp_data.json()
+        except Exception:
+            log.error(traceback.format_exc())
+
+
+    @staticmethod
+    def _get_component_url(components, app_id, component_id):
+        return [d['url'] for d in components if d['app_id'] == app_id and d['component_id'] == component_id][0]
+
+
+    @staticmethod
     def get_data(_request, app_id, component_id, filter_payload=None):
         try:
-            service_url = f"http://kong/{app_id}/report-components/{component_id}"
             headers = {
                 "TenantId": _request.headers.get("TenantId"),
                 "Authorization": _request.headers.get("Authorization"),
             }
+            
+            registry_service_components = ExternalDataService._get_all_components(headers)
+            service_url = ExternalDataService._get_component_url(
+                components=registry_service_components,
+                app_id=app_id,
+                component_id=component_id
+            )
+
             if filter_payload:
                 data = requests.post(
                     url=service_url, headers=headers, json=filter_payload
@@ -24,6 +51,6 @@ class ExternalDataService:
                 log.warning(f"Could not retrieve data for {component_id} from {app_id}")
                 log.warning(f"GET {service_url} {data.status_code}")
                 return []
-        except Exception as e:
-            log.exception(str(e))
+        except Exception:
+            log.error(traceback.format_exc())
             return False
