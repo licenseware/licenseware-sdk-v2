bbcab71aad35df3913bf3ea9fbc5a2705fcd6d02
ClimenteA <climente.alin@gmail.com>
2022-04-04 14:47:31 +0300

    fix: upload event id fetch

--- licenseware/history/event.py
+++ licenseware/history/event.py
@@ -4,12 +4,12 @@ from .func import get_value_from_func
 
 def get_event_id(func, func_args, func_kwargs):
 
-    if func.__name__ == 'validate_filenames':
+    if func.__name__ == "validate_filenames":
         return str(uuid.uuid4())
+
+    if func.__name__ == "upload_files":
+        return func_args[1].args.get("event_id")
 
     event_id = get_value_from_func(func, func_args, func_kwargs, "event_id")
-
-    if event_id is None and func.__name__ == 'upload_files':
-        return str(uuid.uuid4())
 
     return event_id
