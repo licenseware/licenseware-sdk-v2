63b5b077e1d2d13a3138ef3f940139cf92837ec3
ClimenteA <climente.alin@gmail.com>
2022-04-11 09:33:14 +0300

    feat: added mongita for local dev

--- licenseware/mongodata/__init__.py
+++ licenseware/mongodata/__init__.py
@@ -1,13 +1,34 @@
-from .mongodata import (
-    insert, 
-    fetch, 
-    update,
-    delete, 
-    document_count, 
-    delete_collection, 
-    aggregate,
-    get_collection,
-    distinct
-) 
+import os
 
-from .mongo_connection import collection
+
+if os.getenv("ENVIRONMENT") == "desktop":
+
+    from .mongitadata import (
+        insert, 
+        fetch, 
+        update,
+        delete, 
+        document_count, 
+        delete_collection, 
+        aggregate,
+        get_collection,
+        distinct
+    ) 
+
+    from .mongita_connection import collection
+
+else:
+
+    from .mongodata import (
+        insert, 
+        fetch, 
+        update,
+        delete, 
+        document_count, 
+        delete_collection, 
+        aggregate,
+        get_collection,
+        distinct
+    ) 
+
+    from .mongo_connection import collection
---
+++ licenseware/mongodata/mongita_connection.py
@@ -0,0 +1,45 @@
+"""
+
+Function `collection` is a context manager which can be used as folows:
+
+```py
+
+from licensware.mongodata import collection
+
+
+with collection("CollectionName", MarshmallowSchema, data) as col:
+    col.find({}) #any pymongo collection functions
+
+
+```
+
+`MarshmallowSchema` will validate provided `data` provided before inserting it in mongo
+
+
+"""
+
+from mongita import MongitaClientDisk
+from marshmallow import Schema
+from contextlib import contextmanager
+from licenseware.common.constants import envs
+
+
+
+@contextmanager
+def collection(collection_name:str, schema: Schema = None, data:any = None):
+    
+    if schema and data:
+        
+        if isinstance(data, dict):
+            data = schema().load(data)
+
+        if isinstance(data, list):
+            data = schema(many=True).load(data)
+        
+        
+    with MongitaClientDisk(host="./database") as conn: 
+        col = conn[envs.MONGO_DATABASE_NAME][collection_name]
+        try:
+            yield col
+        finally:
+            conn.close()
