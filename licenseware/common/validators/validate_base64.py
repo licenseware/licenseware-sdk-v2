35e41a83de130d1932166a83bea75b4bdae53b6c
ClimenteA <climente.alin@gmail.com>
2022-05-03 10:29:59 +0300

    fix: raise marshmallow exception on failed base64 validation

--- licenseware/common/validators/validate_base64.py
+++ licenseware/common/validators/validate_base64.py
@@ -1,7 +1,8 @@
 import base64
+from marshmallow import ValidationError
 
 
-def validate_base64(sb):
+def _valid_base64(sb: str):
 
 	try:
 		
@@ -12,8 +13,13 @@ def validate_base64(sb):
 			sb_bytes = sb
 		else:
 			raise ValueError("Argument must be string or bytes")
-
+		
 		return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
-
+		
 	except Exception:
 		return False
+
+
+def validate_base64(value: str):	
+	if not _valid_base64(value):
+		raise ValidationError("Not a valid base64 string")
