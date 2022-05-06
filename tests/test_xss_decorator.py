639e0de2f1f3b8f0ee6eedcab066e80c38941654
ClimenteA <climente.alin@gmail.com>
2022-05-06 12:44:09 +0300

    chore: created requirements for xss prevent

--- tests/test_xss_decorator.py
+++ tests/test_xss_decorator.py
@@ -1,38 +1,19 @@
-# import unittest
-# from licenseware
-
-# from flask import request
-# from typing import Callable
-# from functools import wraps
+import unittest
+from licenseware.decorators.xss_decorator import xss_security, xss_validator
 
 
-
-# def xss(fn: Callable = None) -> Callable:
-#     """
-#     Added to flask-restx Api class decorators parameter
-#     This will be applied to each resource and raise an error when suspicious payload is added.
-#     """
-
-#     def decorator(fn: Callable) -> Callable:
-#         @wraps(fn)
-#         def wrapper(*args, **kwargs) -> Callable:
-            
-#             return result
-
-#         return wrapper
-
-#     return decorator(fn) if fn else decorator
+# python3 -m unittest tests/test_xss_decorator.py
 
 
+class TestXSSDecorator(unittest.TestCase):
+
+    def test_xss_validator(self):
+
+        data = {
+            "test": 1
+        }
+
+        xss_validator(data)
 
 
-
-
-
-# # python3 -m unittest tests/test_xss_decorator.py
-
-
-# class TestXSSDecorator(unittest.TestCase):
-
-#     def test_xss_decorator(self):
 
