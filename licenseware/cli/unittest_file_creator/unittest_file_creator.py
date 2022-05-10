ecdcc69b30706febbe237b506ca3a7ad06fccf4a
ClimenteA <climente.alin@gmail.com>
2022-05-10 14:27:47 +0300

    feat: added new cli command licenseware new-unittest name_of_test

---
+++ licenseware/cli/unittest_file_creator/unittest_file_creator.py
@@ -0,0 +1,29 @@
+import os
+from licenseware.cli.base_creator import BaseCreator
+from . import templates
+
+
+
+class UnittestFileCreator(BaseCreator):
+
+    def __init__(self, app_id: str):
+        super().__init__(app_id)
+
+
+    def create(self):
+
+        testfiles_path = f"test_files/{self.entity_underscore}"
+
+        if not os.path.exists(testfiles_path):
+            os.makedirs(testfiles_path)
+
+        fname = "test_" + self.entity_underscore + ".py"
+        
+        if not os.path.exists(os.path.join('./tests', fname)):    
+            self.create_file(
+                filename=fname,  
+                filepath='./tests',
+                template_filename="test_name.py.jinja",
+                template_resource=templates
+            )
+
