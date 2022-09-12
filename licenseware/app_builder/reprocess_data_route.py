bb3d06e52596952c553c82579d019b6c35f6f36d
ClimenteA <climente.alin@gmail.com>
2022-09-12 03:08:07 +0300

    feat: get uploaders from registry-service with machine authorization, skip quota check if authorization is from a machine allowing reprocessing files for a user

--- licenseware/app_builder/reprocess_data_route.py
+++ licenseware/app_builder/reprocess_data_route.py
@@ -4,19 +4,22 @@ This endpoint is called for reprocessing the data by tenant.
 
 """
 
-from flask import request
-from flask_restx import Api, Resource
 import os
 import uuid
+
 import requests
+from flask import request
+from flask_restx import Api, Resource
+from marshmallow import Schema, fields
+
+from licenseware.common.constants import envs
+from licenseware.common.marshmallow_restx_converter import marshmallow_to_restx_model
 from licenseware.decorators import failsafe
 from licenseware.mongodata import aggregate
 from licenseware.report_components import ExternalDataService
-from licenseware.common.constants import envs
-from licenseware.utils.logger import log
 from licenseware.utils.dramatiq_redis_broker import broker
-from marshmallow import Schema, fields
-from licenseware.common.marshmallow_restx_converter import marshmallow_to_restx_model
+from licenseware.utils.logger import log
+
 
 class ReprocessSchema(Schema):
     tenants = fields.List(fields.String, allow_none=True, required=False)
@@ -26,42 +29,22 @@ class ReprocessSchema(Schema):
 def get_uploads(tenant_id):
 
     pipeline = [
+        {"$sort": {"updated_at": 1}},
         {
-            '$sort': {
-                'updated_at': 1
+            "$group": {
+                "_id": ["$tenant_id", "$uploader_id"],
+                "date": {"$last": "$updated_at"},
+                "uploader_id": {"$last": "$uploader_id"},
+                "files_uploaded": {"$last": "$files_uploaded"},
+                "tenant_id": {"$last": "$tenant_id"},
             }
-        }, {
-            '$group': {
-                '_id': [
-                    '$tenant_id', '$uploader_id'
-                ], 
-                'date': {
-                    '$last': '$updated_at'
-                }, 
-                'uploader_id': {
-                    '$last': '$uploader_id'
-                }, 
-                'files_uploaded': {
-                    '$last': '$files_uploaded'
-                },
-                'tenant_id': {
-                    '$last': '$tenant_id'
-                }
-            }
-        }, 
+        },
     ]
 
     if tenant_id is not None:
-        pipeline = [
-            {
-                "$match": {"tenant_id": tenant_id}
-            }
-        ] + pipeline
+        pipeline = [{"$match": {"tenant_id": tenant_id}}] + pipeline
 
-    uploads = aggregate(
-        pipeline,
-        collection=envs.MONGO_COLLECTION_HISTORY_NAME
-    )
+    uploads = aggregate(pipeline, collection=envs.MONGO_COLLECTION_HISTORY_NAME)
     return uploads
 
 
@@ -73,34 +56,29 @@ def get_files(files_uploaded):
     return file_list
 
 
-
 @broker.actor(max_retries=0, queue_name=envs.QUEUE_NAME)
 def send_files(dataset):
 
     auth_headers = {
         "Tenantid": dataset["tenant_id"],
-        "Authorization": envs.get_auth_token()
+        "Authorization": envs.get_auth_token(),
     }
-    
+
     upload_url = ExternalDataService.get_upload_url(
-        _request=auth_headers,
-        app_id=envs.APP_ID,
-        uploader_id=dataset["uploader_id"]
+        _request=auth_headers, app_id=envs.APP_ID, uploader_id=dataset["uploader_id"]
     )
 
     res = requests.post(
-        upload_url,
-        files=get_files(dataset["files_uploaded"]),
-        headers=auth_headers
+        upload_url, files=get_files(dataset["files_uploaded"]), headers=auth_headers
     )
 
     if res.status_code != 200:
         log.warning(res.content)
-        log.error(f"""
+        log.error(
+            f"""
             Failed to send files for uploader_id: {dataset["uploader_id"]} for Tenant: {dataset["tenant_id"]} 
-        """)
-
-
+        """
+        )
 
 
 @broker.actor(max_retries=0, queue_name=envs.QUEUE_NAME)
@@ -110,10 +88,9 @@ def reprocess_files(tenants):
         for dataset in get_uploads(None):
             send_files.send(dataset)
 
-    for tenant_id in tenants:          
+    for tenant_id in tenants:
         for dataset in get_uploads(tenant_id):
             send_files.send(dataset)
-            
 
 
 def add_reprocess_data_route(api: Api, appvars: dict):
@@ -133,10 +110,10 @@ def add_reprocess_data_route(api: Api, appvars: dict):
         )
         @api.expect(model, validate=True)
         def post(self):
-        
+
             if "6202b399-b79b-59ff-9925-5ab1534d324d" != str(
                 uuid.uuid5(uuid.NAMESPACE_DNS, str(request.json["password"]))
-            ):  
+            ):
                 log.warning("Password on `reprocess_files` didn't match")
                 return "Password doesn't match!", 403
 
--- licenseware/report_components/external_data_service.py
+++ licenseware/report_components/external_data_service.py
@@ -38,8 +38,13 @@ class ExternalDataService:
     @staticmethod
     def _get_registry_service_data(headers: dict, endpoint: str) -> list:
         try:
+            auth_headers = (
+                headers
+                if headers["TenantId"] is not None
+                else {"Authorization": headers["Authorization"]}
+            )
             reg_data = requests.get(
-                url=f"{REGISTRY_SERVICE_URL}/{endpoint}", headers=headers
+                url=f"{REGISTRY_SERVICE_URL}/{endpoint}", headers=auth_headers
             )
             return reg_data.json()
         except Exception:
