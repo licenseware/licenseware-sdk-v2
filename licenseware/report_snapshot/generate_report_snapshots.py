5a7f6d07d699105b3e9a1942a24677b7b5a4c857
alincmt <aclimente@ymail.com>
2022-10-06 15:51:10 +0300

    Generate report snapshots (#281)
    * feat: added reprocess data route, updated mongita (mongo on local)
    
    * fix: added registrable bool on appbuiler to tell if app is registrable or not
    
    * feat: generate report snapshots
    
    * chore: lint 🚨
    
    Co-authored-by: ClimenteA <ClimenteA@users.noreply.github.com>

---
+++ licenseware/report_snapshot/generate_report_snapshots.py
@@ -0,0 +1,31 @@
+from licenseware.report_snapshot import ReportSnapshot
+from licenseware.utils.flask_request import get_flask_request
+from licenseware.utils.logger import log
+
+
+def generate_report_snaphosts(tenant_id: str, created_reports):
+
+    for report in created_reports:
+        # Clear current snapshot versions
+        flask_request = get_flask_request(
+            headers={"Tenantid": tenant_id},
+        )
+        rs = ReportSnapshot(report, flask_request)
+        versions = rs.get_available_versions()
+        for version in versions["versions"]:
+            flask_request = get_flask_request(
+                headers={"Tenantid": tenant_id},
+                args={"version": version},
+            )
+            rs = ReportSnapshot(report, flask_request)
+            rs.delete_snapshot()
+
+        # Generate a fresh snapshot
+        flask_request = get_flask_request(
+            headers={"Tenantid": tenant_id},
+            args={"version": version},
+        )
+        rs = ReportSnapshot(report, flask_request)
+        version = rs.generate_snapshot()
+        log.info(f"Report Snaphost created {version['version']}")
+        return version
